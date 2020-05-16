ea24252b6ac53048dc2985b17197e05b57b278f4
v-pis <pisanenko.va@gmail.com>
2020-05-16 16:11:00 +0300

    added game 'guessingame'

---
+++ guessingame.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# File: guessingame.sh
+
+#Get the number of files in the directory
+nfd=$(ls -la | egrep "^-" | wc -l)
+
+#Make a request to the user
+echo "Try to guess how many files are in the current directory ?"
+read nof
+echo ""
+
+#Testing for equality
+while [[ ! $nof -eq $nfd ]]
+do
+
+	if [[ $nof -gt $nfd ]]
+	then
+		echo "Your guess is too high"
+		echo "Try to guess more:"
+        	read nof
+        	echo ""
+	elif [[ $nof -lt $nfd ]]
+	then
+		echo "Your guess is too less"
+		echo "Try to guess more:"
+        	read nof
+        	echo ""
+	fi
+done
+
+#Win
+echo "Congratulations! You guessed it."
+
